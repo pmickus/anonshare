527453d04232a29552fa23eb07d097cc7aaa79f2
Paul Mickus <pmickus@gmail.com>
2015-03-28 01:00:54 -0400

    s3 bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
    :storage => :s3,
     :s3_credentials => {
-      :bucket => "ENV['S3_BUCKET']"
+      :bucket => ENV['S3_BUCKET']
   }
 }
 end
