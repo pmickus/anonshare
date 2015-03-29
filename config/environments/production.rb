34f301520ad3b7d25f18d04662eae3912acd9e38
Paul Mickus <pmickus@gmail.com>
2015-03-29 18:23:13 -0400

    change cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET']
      },
-    :s3_host_alias => 'content.anonshare.us',
+    :s3_host_alias => 'cdn.anonshare.us',
     :url => ':s3_alias_url',
     :path => ":class/:attachment/:id_partition/:style/:filename",
     :bucket => ENV['S3_BUCKET']
