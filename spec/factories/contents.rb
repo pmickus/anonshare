9b302dd87fe5af1b4cbd697e7df8b7579d697590
Paul Mickus <pmickus@gmail.com>
2015-03-31 16:28:20 -0400

    start fixing specs

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -1,6 +1,8 @@
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
   factory :content do
-    file_type ""
-    uploaded_by "MyString"
+    uploaded_by "127.0.0.1"
+    file { fixture_file_upload(Rails.root.join('spec/fixtures/ruby.png'), 'image/png') }
   end
 end
